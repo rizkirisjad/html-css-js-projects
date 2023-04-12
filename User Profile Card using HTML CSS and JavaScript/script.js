07364d3e21962d52569d1173022a04d4c86064f3
rizkirisjad <rizkirisjad@gmail.com>
2023-04-12 22:47:51 +0700

    menambah file baru

---
+++ User Profile Card using HTML CSS and JavaScript/script.js
@@ -0,0 +1,11 @@
+/** @format */
+
+const toggleHeart = document.getElementById('heart');
+toggleHeart.addEventListener('click', () => {
+  toggleHeart.classList.toggle('fa-solid');
+  if (toggleHeart.classList == 'fa-regular fa-heart fa-solid') {
+    toggleHeart.style.color = 'red';
+  } else {
+    toggleHeart.style.color = 'white';
+  }
+});
