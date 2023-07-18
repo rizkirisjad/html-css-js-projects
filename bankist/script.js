0bcc27d53d5589a5755666f2c1d51a24902723f7
rizkirisjad <rizkirisjad@gmail.com>
2023-07-18 18:28:38 +0700

    menambah function

--- bankist/script.js
+++ bankist/script.js
@@ -78,6 +78,19 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsernames = function (accounts) {
+  accounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
